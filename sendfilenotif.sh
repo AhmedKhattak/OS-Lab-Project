1297288327f2e992939da927729806cc24ff3f62
Ahmed Khattak <khattak.ahmed@yahoo.com>
2016-05-23 20:06:48 +0500

    added checks

--- sendfilenotif.sh
+++ sendfilenotif.sh
@@ -4,20 +4,27 @@
 #Gmail settings must be configured before using this script
 #this script has not been tested in networks behind proxies
 #find largest file by sorting within the limit here limit is 100mb in bytes because of poisx rules
-FILES=$(find . -maxdepth 1  -size +102400000c -printf 'File : %p  Size: %s\n'|sort -nr|head -n  1)
+FILES="" #zero length variable needed for checking in later steps
+FILES=$(find . -maxdepth 1 -name "video.mp4" -size +102400000c -printf 'File : %p  Size: %s\n'|sort -nr|head -n  1)
 #file_details=$(echo $FILES | tr " " "\n")
 # script to send email using ssmpt
+# Email text/message
+if [ -n "$FILES" ]; then  #checks that the string is non zero length it has been initialized as zero length
 EMAILMESSAGE="/tmp/emailmessage.txt" # create file in temp directory
-#the mail used below the To part must be same as the one  used in the ssmtp line
-echo "To: recpientmail@somemail.com 
-From: yourmail@somemail.com"
+echo "To: recipient@yahoo.com
+From: sender@gmail.com
 Subject: Sent from a terminal!
 
 $FILES has exceeded its size limit please deal with it.
 "> $EMAILMESSAGE
 echo "End Transmission" >>$EMAILMESSAGE
 echo "Sending..."
-echo "\n\n" >>$EMAILMESSAGE #need new line to simulate ctrl-d as it is required to send the mail from ssmtp
+echo $EMAILMESSAGE
+cat  $EMAILMESSAGE #show the email message
+echo "\n\n" >>$EMAILMESSAGE #need new line to simulatectrl-d as it is required to send the mail from ssmtp
 #this mail here notice the ssmtp and the one in the message is same
-ssmtp "recpientmail@somemail.com"  < $EMAILMESSAGE #send input to ssmtp to send email
-echo "Done." 
+ssmtp "recipient@yahoo.com"  < $EMAILMESSAGE #send input to ssmtp to send email
+echo "ssmtp execution completed" #if there is any further output it will be displayed by ssmtp 
+else
+echo "Condition not set email not sent." 
+fi
